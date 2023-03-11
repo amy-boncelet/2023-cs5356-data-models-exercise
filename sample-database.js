cb069b99d8ceeb5e008269d1f376422133a91bc5
amy-boncelet <ajb347@cornell.edu>
2023-03-11 07:40:44 -0500

    Create sample-database.js

---
+++ sample-database.js
@@ -0,0 +1,14 @@
+{
+    "classes": [{
+        "name": "CS 5356",
+        "sessions": [{
+            "id": "123456",
+            "questions": [{
+                "name": "Danny",
+                "question": "why is the sky blue",
+                "sessionId"
+            }]
+        }]
+    }],
+    "questions": []
+}
