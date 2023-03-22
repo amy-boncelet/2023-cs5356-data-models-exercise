bdb21d35ec51e59eede57252fd103c36cbf42600
amy-boncelet <ajb347@cornell.edu>
2023-03-22 09:33:46 -0400

    class notes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 let database = {
     classes: [],
     questions: []
@@ -6,29 +7,55 @@ let database = {
 const getRandomNumber = () => {
     return `${Math.floor(Math.random() * 900000) + 100000}`
 }
+=======
+const database = {
+    classes : [],
+    questions: []
+}
+
+// const getRandomNumber = () => {
+//     return `${Math.floor(Math.random()*900000) + 100000}`
+// }
+>>>>>>> Stashed changes
 
 export const createClass = (className) => {
     database.classes.push({
         className: className
     })
 }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 export const getClasses = () => {
     return database.classes
 }
 
 export const createClassSession = className => {
+<<<<<<< Updated upstream
     // find the class with the class name
+=======
+    // need to find the class with the class name
+>>>>>>> Stashed changes
     const classInfo = database.classes.find(classInfo => {
         return classInfo.className === className
     })
 
     classInfo.sessions = []
+<<<<<<< Updated upstream
     classInfo.sessions.push(getRandomNumber())
 }
 
 export const getClassSessions = className => {
     // find the class with the class name
+=======
+    // classInfo.sessions.push(getRandomNumber())
+    classInfo.sessions.push('123456')
+}
+
+export const getClassSessions = className => {
+    // need to find the class with the class name
+>>>>>>> Stashed changes
     const classInfo = database.classes.find(classInfo => {
         return classInfo.className === className
     })
@@ -56,3 +83,11 @@ export const clear = () => {
         questions: []
     }
 }
+<<<<<<< Updated upstream
+=======
+
+// export is a way of exporting something out of the file
+// const helloWorld = 'helloWorld'
+// export default helloWorld;
+// note - can only have one default value
+>>>>>>> Stashed changes
--- index.spec.js
+++ index.spec.js
@@ -1,10 +1,22 @@
 import * as db from './index.js'
+// or can write import {getQuestions} from './index.js'
+// second line only grabs the {getQuestions} function
+// while first line, * would get everything from the file. it all gets dumped into object called db
+
+// or can also write import anything from './index.js' (anything can be any word)
+// this will export a default export statement
+
 
 describe('class questions app', () => {
+<<<<<<< Updated upstream
+=======
+    
+>>>>>>> Stashed changes
     afterEach(() => {
         db.clear();
     })
 
+<<<<<<< Updated upstream
     describe('Classes', () => {
         it('creates a class', () => {
             const className = 'CS 5356'
@@ -24,6 +36,25 @@ describe('class questions app', () => {
             const classSessions = db.getClassSessions(className)
             expect(classSessions.length).toEqual(1)
         })    
+=======
+    it('creates a class', () => {
+        const className = 'CS 5356'
+        db.createClass(className)
+
+        const classes = db.getClasses()
+        expect(classes.length).toEqual(1)
+        expect(classes[0]).toEqual({className: 'CS 5356'})
+    })
+
+    it('creates a class session for a class', () => {
+        const className = 'CS 5356'
+        db.createClass(className)
+
+        db.createClassSession(className)
+
+        const classSessions = db.getClassSessions(className)
+        expect(classSessions.length).toEqual(1)
+>>>>>>> Stashed changes
     })
 
     it('asks a question', () => {
@@ -35,11 +66,16 @@ describe('class questions app', () => {
         const classSessions = db.getClassSessions(className)
 
         let name = 'user123'
+<<<<<<< Updated upstream
         let question = 'Why is the sky blue?'
+=======
+        let question = 'why is the sky blue?'
+>>>>>>> Stashed changes
         let classSession = classSessions[0]
         db.askQuestion(name, question, classSession)
 
         name = 'user456'
+<<<<<<< Updated upstream
         question = 'A very random question'
         classSession = classSessions[1]
         db.askQuestion(name, question, classSession)
@@ -47,5 +83,14 @@ describe('class questions app', () => {
         const questions = db.getQuestions(classSessions[0])
         expect(questions.length).toEqual(1)
         expect(questions[0].question).toEqual('Why is the sky blue?')
+=======
+        question = 'what time is it?'
+        classSession = classSessions[1]
+        db.askQuestion(name, question, classSession)
+
+        const questions = db.getQuestions(classSession[0])
+        expect(questions.length).toEqual(1)
+        expect(questions[0].question).toEqual('why is the sky blue?')
+>>>>>>> Stashed changes
     })
 })
